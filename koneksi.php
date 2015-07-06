8dbf1959970dcf49421508f18216e00af6ff5e72
lexsofahmi@gmail.com <lexsofahmi@gmail.com>
2015-07-06 23:17:05 +0700

    demo_mysqli

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+
+include "services/DatabaseUtil.php";
+
+DatabaseUtil::setConnectServer('127.0.0.1', 'root', '');
+
+DatabaseUtil::getConnectServer();
---
+++ services/DatabaseUtil.php
@@ -0,0 +1,25 @@
+<?php
+
+class DatabaseUtil {
+    private static $dbHost;
+    private static $dbUser;
+    private static $dbPass;
+    private static $dbName;
+
+    public static function setConnectServer($dbHost, $dbUser, $dbPass) {
+        self::$dbHost = $dbHost;
+        self::$dbUser = $dbUser;
+        self::$dbPass = $dbPass;
+    }
+
+    public static function getConnectServer() {
+        $conn = mysqli_connect(self::$dbHost, self::$dbUser, self::$dbPass);
+
+        try {
+            if (!$conn) throw new Exception("Gak connect");
+            else echo "Success <br/>";
+        } catch (Exception $conn) {
+            echo $conn;
+        }
+    }
+}
